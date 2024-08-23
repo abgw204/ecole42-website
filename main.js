f90a99a4d46d5980cd422b9681a8763c8ddf6488
Gabriel <elementalgames77@gmail.com>
2024-08-22 21:14:54 -0300

    close button added

--- main.js
+++ main.js
@@ -6,9 +6,11 @@ menuIcon.addEventListener('click', ()=>
     if (ul.classList.contains('active'))
     {
         ul.classList.remove('active');
+        document.querySelector('.menu-icon img').src = 'assets/menu-icon-white.png';
     }
     else 
     {
-        ul.classList.add('active')
+        ul.classList.add('active');
+        document.querySelector('.menu-icon img').src = 'assets/close-btn.png';
     }
     })
